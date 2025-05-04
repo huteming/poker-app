2a28b2f59d69677ea5bc7c94dea469ba0681b434
Teming.Hu <teming.hu@qq.com>
2025-05-04 19:27:12 +0800

    fix: 修复线上请求地址错误

--- lib/services/base_service.dart
+++ lib/services/base_service.dart
@@ -5,8 +5,8 @@ import 'package:poker/models/db_error.dart';
 
 abstract class BaseService {
   final log = Logger('BaseService');
-  // final String _baseUrl = 'https://api.huteming.fun/poker/v1';
-  final String _baseUrl = 'http://127.0.0.1:8787/poker/v1';
+  final String _baseUrl = 'https://api.huteming.fun/poker/v1';
+  // final String _baseUrl = 'http://127.0.0.1:8787/poker/v1';
   final String _token = '1234567890';
 
   String get baseUrl => _baseUrl;
